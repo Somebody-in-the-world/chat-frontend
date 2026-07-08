15d7760c44fda5237d0384cae3380dc24ac27158
Somebody-in-the-world <>
2026-07-08 23:06:05 +0800

    Fix vue router

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,6 +19,6 @@ const routes: RouteRecordRaw[] = [
 ];
 
 export const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes
 });
